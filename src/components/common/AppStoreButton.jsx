78f6b7fb9e30f29a414dfe18f92f99d31a0fd5ef
Amit-1610 <amitkinha2000@gmail.com>
2026-01-30 22:05:25 +0530

    third commit

--- src/components/common/AppStoreButton.jsx
+++ src/components/common/AppStoreButton.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import playstore from "../../assets/playstore.png";
+import appstoreimg from "../../assets/appstore.png";
 
 const AppStoreButton = ({
     type = "playstore",
@@ -12,14 +14,14 @@ const AppStoreButton = ({
                 return (
                     <>
                         {showText && <span className="download_text">{text}</span>}
-                        <img src="/src/assets/playstore.png" alt="Google Play Store" />
+                        <img src={playstore} alt="Google Play Store" />
                     </>
                 );
             case "appstore":
                 return (
                     <>
                         {showText && <span className="download_text">{text}</span>}
-                        <img src="/src/assets/appstore.png" alt="App Store" />
+                        <img src={appstoreimg} alt="App Store" />
                     </>
                 );
             default:
